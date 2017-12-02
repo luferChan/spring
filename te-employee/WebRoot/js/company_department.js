3f23dc0c296a815b038836e19704bb565f901182
luferChan <chyou@vip.qq.com>
2017-12-02 20:32:40 +0800

    对从后台获取到的json数据在js中进行遍历展示，并实现模糊查询功能

--- te-employee/WebRoot/js/company_department.js
+++ te-employee/WebRoot/js/company_department.js
@@ -6,6 +6,10 @@ $(function(){
 	$('button.btn-save').on('click',function(e){
 		depm.save();
 	});
+	$('button.btn-search').on('click',function(e){
+		depm.findDepartmentList(1);
+	});
+	depm.findDepartmentList(1);
 });
 
 var depm = {
@@ -31,5 +35,35 @@ var depm = {
 	
 	cleanMsg : function(){
 		$('input.dep-name-input,textarea.dep-desc-text').val("");
+	},
+	
+	findDepartmentList : function(page){
+		var search_name = $('input.text-search').val();
+		var tbody = $('tbody.tbody-list').empty();
+		$.post('./mgr/0/department/findDepartmentList',
+				{
+					search_name : search_name, 
+					page : page
+					
+				},function(data){
+					if(!$.isSuccess(data)) return;
+					$.each(data.body,function(index,value){
+						$('<tr></tr>')
+						.append($("<td></td>").append(value.id))
+						.append($("<td></td>").append(value.name))
+						.append($("<td></td>").append(value.creatTime))
+						.append($("<td></td>").append(value.creator))
+						.append($("<td></td>").append(value.description))
+						.append($("<td></td>").append(depm.getBtns(index,value))).appendTo(tbody);
+					});
+			
+				});
+	},
+	
+	getBtns : function(index,value){
+		var btns = "";
+		btns += "<button type='button' class='btn btn-primary btn-xs'><span class='glyphicon glyphicon-check' aria-hidden='true'></span>&nbsp;编辑</button>";
+		btns += "<button type='button' class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span>&nbsp;删除</button>";
+		return btns;
 	},
 };
