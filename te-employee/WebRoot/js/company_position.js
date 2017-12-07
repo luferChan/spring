5497e8db8e7bfa0917e08704beacd7d76e42cd02
luferChan <chyou@vip.qq.com>
2017-12-07 10:44:20 +0800

    完成添加职位信息时的部门信息下拉菜单框的数据显示

--- te-employee/WebRoot/js/company_position.js
+++ te-employee/WebRoot/js/company_position.js
@@ -1,3 +1,34 @@
 $(function(){
 	$.getMenu();
+	position.init();
+	
 });
+
+var position = {
+	
+	init : function(){
+		$('button.btn-newPosition').on('click',function(e){
+			position.showAddBox();
+		});
+	},
+	
+	showAddBox : function(){
+		position.getSelectDepartment();
+		Dialog.showModal('#addPosiModal');
+	},
+	
+	getSelectDepartment : function(){
+		var select = $('select.add-posi-select').empty().append($('<option value="0"></option>').append("请选择部门..."));
+		$.post('./mgr/0/position/getSelectDepartment',
+				{
+				
+				},function(data){
+					if(!$.isSuccess(data)) return;
+					//console.log(data.body);
+					$.each(data.body,function(index,value){
+						$("<option value="+value.id+"></option>").append(value.name).appendTo(select);
+					});
+			
+				});
+	},
+};
